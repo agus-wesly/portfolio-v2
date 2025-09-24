22b11e0c7409f0b25c02cc759669255c119c6e2a
aguswesly <agustchannel@gmail.com>
2025-09-24 10:40:26 +0700

    feat: configure blog folder setup & add test blog

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,29 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from "node:path"
+import { fileURLToPath } from "node:url"
+import  fs  from "node:fs"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const blogFiles = await fs.promises.readdir("./blogs");
+const blogInput = {}
+for (const blogFileName of blogFiles) {
+    const name = `blogs/${blogFileName}`
+    blogInput[name] = resolve(__dirname, name)
+}
 
 export default defineConfig({
-  base: '/portfolio-v2/',
+    base: '/portfolio-v2/',
+    plugins: [
+        tailwindcss()
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                ...blogInput
+            },
+        }
+    }
 })
