dc4514c62349e905057fe3d9175941ae1ffc9760
aguswesly <agustchannel@gmail.com>
2025-09-28 20:54:38 +0700

    fix: separate module & display hero image when ready

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,11 @@
-import './style.css'
-
-const menuButton = document.getElementById("menu-btn") as HTMLButtonElement
-const menuPopover = document.getElementById("menu-popover") as HTMLDivElement
-
-function closeMenuPopover() {
-    menuPopover.style.opacity = "0"
-    menuPopover.style.pointerEvents = "none"
-    menuPopover.blur()
-    removeClickOutsideListener(menuPopover)
-}
-
-function openMenuPopover() {
-    menuPopover.style.pointerEvents = "auto"
-    menuPopover.style.opacity = "1"
-    menuPopover.focus()
-    addClickOutsideListener(menuPopover, () => {
-        closeMenuPopover()
-    })
-
-}
-
-menuButton.onclick = () => {
-    const isVisible = menuPopover.style.pointerEvents === "auto"
-    if (isVisible) {
-        closeMenuPopover()
-    } else {
-        openMenuPopover()
+const imgEl = document.getElementById("hero-img") as HTMLImageElement
+function main() {
+    const img = new Image()
+    img.onload = () => {
+        imgEl.src = img.src
+        imgEl.style.display = "block"
     }
+    img.src = "images/sullyoon.gif"
 }
 
-menuPopover.addEventListener("keydown", (e) => {
-    if (e.code === "Escape") {
-        closeMenuPopover()
-    }
-})
-
-const onClickOutsideCbMap = new WeakMap<HTMLElement, (e: MouseEvent) => void>;
-
-function addClickOutsideListener(el: HTMLElement, cb: () => void) {
-    const fn = (e: MouseEvent) => {
-        const bound = el.getBoundingClientRect()
-        const isWithin = e.clientX > bound.left && e.clientX < bound.right 
-            && e.clientY > bound.top && e.clientY < bound.bottom
-        if (!isWithin) {
-            cb()
-        } 
-    }
-
-    const skipFirstClick = () => {
-        document.removeEventListener("click", skipFirstClick);
-        document.addEventListener("click", fn);
-        onClickOutsideCbMap.set(el, fn);
-    };
-
-    document.addEventListener("click", skipFirstClick, { once: true });
-}
-
-function removeClickOutsideListener(el: HTMLElement){
-    const stuff = onClickOutsideCbMap.get(el)
-    if (!stuff) throw new Error("Should have cb")
-    document.removeEventListener("click", stuff)
-}
-
+main()
