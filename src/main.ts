976651bf3bedae93c5e40b20df000c03a04f670f
aguswesly <agustchannel@gmail.com>
2025-09-29 08:28:11 +0700

    fix: enable again

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 const imgEl = document.getElementById("hero-img") as HTMLImageElement
 function main() {
-    return
     const img = new Image()
     img.onload = () => {
         imgEl.src = img.src
