a5c573016b2ab9d253417fe7ec6dabf6a8967cbd
aguswesly <agustchannel@gmail.com>
2025-09-29 08:25:55 +0700

    fix: try to disable the gif

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 const imgEl = document.getElementById("hero-img") as HTMLImageElement
 function main() {
+    return
     const img = new Image()
     img.onload = () => {
         imgEl.src = img.src
