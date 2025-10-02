dfe73f67f4de7c7327010e2fb81bce8f63d42a16
aguswesly <agustchannel@gmail.com>
2025-10-02 10:46:10 +0700

    feat: add all blog page

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
+                blog: resolve(__dirname, "blog.html"),
                 ...blogInput
             },
         }
