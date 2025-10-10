ac73e8f0ecada09277102f70c5c35114547ea756
Wesly <agustchannel@gmail.com>
2025-10-10 19:27:22 +0700

    fix: file format

--- vite.config.ts
+++ vite.config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
                 blog: resolve(__dirname, "blog.html"),
                 project: resolve(__dirname, "project.html"),
                 /* In the future we can make project also dynamic ?? */
-                ["projects/cws-language"]: resolve(__dirname, "projects/cws-language"),
-                ["projects/raycasting-from-scratch"]: resolve(__dirname, "projects/raycasting-from-scratch"),
+                ["projects/cws-language"]: resolve(__dirname, "projects/cws-language.html"),
+                ["projects/raycasting-from-scratch"]: resolve(__dirname, "projects/raycasting-from-scratch.html"),
                 ...blogInput
             },
         }
